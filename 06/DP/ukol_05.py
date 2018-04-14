41ca7352e36668422fdfdcc93e37fad5a801544a
Míša R <michaela.radova@email.cz>
2018-04-14 23:50:42 +0200

    Dopsany komentare do ukol_05.py a ukol_06.py.

--- 06/DP/ukol_05.py
+++ 06/DP/ukol_05.py
@@ -1,2 +1,4 @@
+"""Program vypise 4 radky, na kazdem je napsano 'a'."""
+
 for i in range(4):
     print("a")
--- 06/DP/ukol_06.py
+++ 06/DP/ukol_06.py
@@ -1,2 +1,4 @@
+"""Program vypise 5 radku s napisem Radek a cislo radku."""
+
 for cislo_radku in range(5):
     print("Řádek", cislo_radku)
