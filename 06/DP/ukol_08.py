1b0f2326707eee457d54fef95e6ebd219822ec99
Míša R <michaela.radova@email.cz>
2018-04-14 23:46:13 +0200

    Pridan soubor ukol_08.py - program vypise 5 radku, na kazdy napise jaka je 2. mocnina cisla, kdy cisla jdou od 0 do 4.

---
+++ 06/DP/ukol_08.py
@@ -0,0 +1,6 @@
+"""program vypise 5 radku, na kazdy napise jaka je 2. mocnina cisla, kdy cisla
+   jdou od 0 do 4"""
+
+for mocnenec in range(5):
+    mocnina = mocnenec ** 2
+    print(mocnenec, "na druhou je", mocnina)
