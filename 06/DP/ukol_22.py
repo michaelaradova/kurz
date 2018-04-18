c844ad2da583660090026afaa3b24babc0f9f640
Míša R <michaela.radova@email.cz>
2018-04-18 22:41:01 +0200

    V ukol_22.py uprava fce pro stanoveni, zda je cislo prvocislo - print hodnoceni dan 	mimo fci samotnou, az k jejimu volani.

--- 06/DP/ukol_22.py
+++ 06/DP/ukol_22.py
@@ -13,7 +13,7 @@ def faktorial(cislo):
     return faktorial_cisla
 
 
-def prvocislo(cislo):
+def je_prvocislo(cislo):
     """Urci, zda je zadane cislo prvocislo"""
     # zda je cislo prvocislo se da overit tim, ze se overi, ze neni delitelne
     # zadnym cislem od 2 do odmocniny z toho cisla
@@ -22,8 +22,7 @@ def prvocislo(cislo):
     delitel = 2
     zbytek_deleni = cislo % delitel
     if zbytek_deleni == 0:
-        print("{} neni prvocislo. Je delitelne {}.".format(cislo, delitel))
-        return False  # pokuj delitelne 2, neni prvocislo
+        return False, delitel  # pokud delitelne 2, neni prvocislo
     # zjistovani, zda je cislo delitelne nejakym lichym cislem az do odmocniny
     # daneho cisla
     delitel = 3
@@ -31,11 +30,10 @@ def prvocislo(cislo):
         zbytek_deleni = cislo % delitel
         delitel = delitel + 2  # + 2, protoze testuji jiz jen licha cisla
     if zbytek_deleni == 0:
-        print("{} neni prvocislo. Je delitelne {}.".format(cislo, delitel-2))
-        return False
+        return False, delitel - 2  # nalezen delitel, neni prvocislo
     else:
-        print("{} je prvocislo.".format(cislo))
-        return True
+        delitel = None
+        return True, delitel  # nenalezen zadny delitel, je prvocislo
 
 
 def Fibonacciho_posloupnost(pocet_cisel):
@@ -61,7 +59,11 @@ if __name__ == "__main__":
     faktorial_x = faktorial(cislo_x)
     print("Faktorial cisla {} je: {}".format(cislo_x, faktorial_x))
     print()  # prazdna radka pro prehlednost vypisu
-    je_prvocislo = prvocislo(cislo_x)
+    je_prvocis, delitel = je_prvocislo(cislo_x)
+    if je_prvocis:
+        print("{} je prvocislo.".format(cislo_x))
+    else:
+        print("{} neni prvocislo. Je delitelne {}.".format(cislo_x, delitel))
     print()  # prazdna radka pro prehlednost vypisu
     Fib_posloupnost = Fibonacciho_posloupnost(pocet_cisel=cislo_x)
     print("Prvnich {} clenu Fibonacciho posloupnosti je: {}".format(cislo_x,
